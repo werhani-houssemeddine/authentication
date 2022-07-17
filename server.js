5d4610a662ec353efcc3fad3350434e3f6303b17
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2022-07-17 10:08:59 +0100

    I configue the login page, i cancel using the redis module due to an error until i fix that error, I set the session cookies in the next step I will fix the sign up page

--- server.js
+++ server.js
@@ -1,18 +1,66 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const session = require('express-session');
-const {createClient} = require('redis');
+const redis = require('redis');
 const Redis = require('connect-redis')(session);
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+// Import local files
+const handler = require('./lib/handler');
+const redis_db = require('./lib/db_file');
+const User = require('./Model/users');
 
 const app = express();
 const port = process.env.PORT || 3000;
+redis_db;
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    saveUninitialized: false,
+    resave: false,
+    secret: 'glblblhhcf',
+    /*store: new Redis({
+        url: '127.0.0.1:6379',
+        client: redis_db,
+        logErrors: true
+    })*/
+}));
+
+mongoose.connect('mongodb://localhost:27017/first_db');
+const mongodb = mongoose.connection;
+
+mongodb.on('error', (err) => console.error(err));
+
+// Set up handlebars engine
 app.set('view engine', 'handlebars');
-
 app.engine('handlebars', handlebars.engine({
-    defaultLayout: 'main'
-}))
+    defaultLayout: 'main',
+}));
 
-app.get('/', (req, res) => res.render('home'));
 
-app.listen(port, () => console.log(`Listenning on port ${port}`));
+app.get('/', handler.home);
+app.get('/signUp', handler.signUp);
+
+app.post('/add', async (req, res) => {
+    const {name, email, password} = req.body;
+    const isExist = await User.findOne({email});
+    if(isExist){
+        console.log('☹️ ☹️ ☹️');
+        res.redirect('/signUp');
+        res.status(300);
+    }else{
+        User.create({name, email, password});
+        console.log('😊 😊 😊');
+        res.redirect('/');
+        res.status(300);
+    }
+});
+
+app.post('/api/login', handler.api.processLogin);
+
+mongodb.once('open', () => app.listen(port, () => console.log(`Listenning on port ${port}`)));
